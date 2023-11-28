f7582159b60188898a0504263e3484c5cf33da03
Franz Genegobis <genegobis.franz@gmail.com>
2023-11-28 23:03:25 +0800

    Summary: The filter feature associated with the Venn Diagram

--- script/Homepage.js
+++ script/Homepage.js
@@ -1,47 +1,50 @@
-            document.addEventListener('DOMContentLoaded', function() {
-                const buttons = document.querySelectorAll('.buttons input[type="button"]');
-                
-                buttons.forEach(btn => {
-                    if (btn.value === 'Home') {
-                        btn.classList.add('button-clicked');
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll('.buttons input[type="button"]');
+
+    buttons.forEach((btn) => {
+        if (btn.value === "Home") {
+            btn.classList.add("button-clicked");
+        }
+    });
+
+    function handleButtonClick() {
+        buttons.forEach((btn) => {
+            btn.addEventListener("click", function () {
+                buttons.forEach((b) => {
+                    if (b !== this) {
+                        b.classList.remove("button-clicked");
+                        b.classList.add("button1");
                     }
                 });
-    
-                function handleButtonClick() {
-                    buttons.forEach(btn => {
-                        btn.addEventListener('click', function() {
-                            buttons.forEach(b => {
-                                if (b !== this) {
-                                    b.classList.remove('button-clicked');
-                                    b.classList.add('button1');
-                                }
-                            });
-    
-                            if (!this.classList.contains('button-clicked')) {
-                                this.classList.remove('button1');
-                                this.classList.add('button-clicked');
-                            } else {
-                                this.classList.remove('button-clicked');
-                                this.classList.add('button1');
-                            }
-                        });
-                    });
+
+                if (!this.classList.contains("button-clicked")) {
+                    this.classList.remove("button1");
+                    this.classList.add("button-clicked");
+                } else {
+                    this.classList.remove("button-clicked");
+                    this.classList.add("button1");
                 }
-    
-                handleButtonClick();
             });
+        });
+    }
 
-  const popupButton = document.getElementById('pop-up');
-  const overlay = document.getElementById('overlay');
-  const popup = document.getElementById('popup');
-  const closeButton = document.getElementById('closeButton');
+    handleButtonClick();
+});
 
-  popupButton.addEventListener('click', function() {
-    overlay.style.display = 'block';
-    popup.style.display = 'block';
-  });
+<!-- Your HTML content -->
 
-  closeButton.addEventListener('click', function() {
-    overlay.style.display = 'none';
-    popup.style.display = 'none';
-  });
+
+const popupButton = document.getElementById("pop-up");
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementById("closeButton");
+
+popupButton.addEventListener("click", function () {
+    overlay.style.display = "block";
+    popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", function () {
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
