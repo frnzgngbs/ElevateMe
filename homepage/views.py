742f0f9fda5eaeb3f43a8e35c6a7f5b9f4429dfa
Franz Genegobis <genegobis.franz@gmail.com>
2023-11-30 21:06:56 +0800

    Summary: Changes the api key and added checkbox

--- homepage/views.py
+++ homepage/views.py
@@ -64,7 +64,7 @@ class GeneratePS(View):
 
 
 def generateAi(field1, field2, field3, filter):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     if filter != "":
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{
